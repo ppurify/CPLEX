9fac4f063053c9b11421f1eee653c0606c791de4
purify <qpqp8558@gmail.com>
2022-05-04 16:20:28 +0900

    Linear Programming Example2_1_1

---
+++ LinearProgramming/Example2_1_1.py
@@ -0,0 +1,9 @@
+import scipy.optimize as so
+import numpy as np
+
+A = np.array([[6, 4], [1, 2], [-1, 1], [0, 1]])
+b = np.array([24, 6, 1, 2])
+c = np.array([-5, -4])
+
+result = so.linprog(c, A, b, method='simplex')
+print(result)
