5c8f45460e2d2a34929b6c0f1234efe8e50f084f
purify <qpqp8558@gmail.com>
2022-05-04 20:22:13 +0900

    Linear Programming

--- LinearProgramming/Example2_1.py
+++ LinearProgramming/Example2_1.py
@@ -1,12 +1,29 @@
-import scipy.optimize as so
-import numpy as np
+from docplex.mp.model import Model
 
-# Example2.1-1
-A = np.array([[6, 4], [1, 2], [-1, 1], [0, 1]])
-b = np.array([24, 6, 1, 2])
-c = np.array([-5, -4])
+# create one model instance, with a name
+m = Model(name='Practice1')
 
-result = so.linprog(c, A, b, method='simplex')
-print(result)
+# by default, all variables in Docplex have a lower bound of 0 and infinite upper bound
+x1 = m.continuous_var(name='Exterior')
+x2 = m.continuous_var(name='Interior')
+
+# write constraints
+# constraint_1: Usage of raw material M1 by both paints
+m.add_constraint(6 * x1 + 4 * x2 <= 24)
+# constraint_2: Usage of raw material M2 by both paints
+m.add_constraint(x1 + 2 * x2 <= 6)
+# constraint_3: Market limit
+m.add_constraint( x2 -x1  <= 1)
+# constraint_4: Interior is lesser than 2 ; # Demand limit
+m.add_constraint(x2 <= 2)
+
+m.maximize(5 * x1 + 4 * x2)
+
+# Print information about the model
+# m.print_information()
+
+# Solve with the model
+s = m.solve()
+m.print_solution()
 
 # Example2.2-2
